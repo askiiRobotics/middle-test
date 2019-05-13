55dd60cb6e2058564b8bf2c9b6e860af7f68cab3
Aleksandr Belets <askii.robotics@gmail.com>
2019-05-13 11:49:33 +0300

    initial commit with application core

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+'use strict';
+
+import {AppRegistry} from 'react-native';
+import App from 'middle/src/App';
+import {name as appName} from 'middle/app.json';
+
+AppRegistry.registerComponent(appName, () => App);
