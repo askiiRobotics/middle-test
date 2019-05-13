b2366a2d1d0181ec5f1a2fd36fce8a0dae043757
Aleksandr Belets <askii.robotics@gmail.com>
2019-05-13 12:48:23 +0300

    updated form selector

--- src/selectors/RootComponentSelector.js
+++ src/selectors/RootComponentSelector.js
@@ -12,6 +12,11 @@ import { formIdList } from 'middle/src/predefined/constants';
 
 const selector = formValueSelector(formIdList.mainForm);
 const RootComponentSelector = createStructuredSelector({
-  firstValue: (state) => selector(state, 'firstValue')
+  'avatar': (state) => selector(state, 'avatar'),
+  'first-name': (state) => selector(state, 'first-name'),
+  'last-name': (state) => selector(state, 'last-name'),
+  'phone': (state) => selector(state, 'phone'),
+  'email': (state) => selector(state, 'email'),
+  'telegram': (state) => selector(state, 'telegram'),
 });
 export default RootComponentSelector;
