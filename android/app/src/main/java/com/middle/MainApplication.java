8c66737311f1c77332013f125e0d99ce27007a2c
Aleksandr Belets <askii.robotics@gmail.com>
2019-05-28 19:14:19 +0300

    configured native part for android

--- android/app/src/main/java/com/middle/MainApplication.java
+++ android/app/src/main/java/com/middle/MainApplication.java
@@ -3,6 +3,9 @@ package com.middle;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new VectorIconsPackage(),
+            new PickerPackage(),
+            new RNFetchBlobPackage()
       );
     }
 
