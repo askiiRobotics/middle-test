665f6b9abecf98f84e71857a0ac4eef2b0920c73
Aleksandr Belets <askii.robotics@gmail.com>
2019-05-28 19:14:31 +0300

    configured babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', 'flow'],
+  presets: [
+    '@babel/preset-flow',
+    '@babel/preset-env',
+    '@babel/react',
+    'module:metro-react-native-babel-preset'
+  ],
 };
