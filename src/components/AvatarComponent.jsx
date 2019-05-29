19c6ee14e5d0f032195b2ed429f062cdf84fa272
alex b <ab@gbooking.co>
2019-05-29 21:22:52 +0300

    fixed saving logic in avatar component

--- src/components/AvatarComponent.jsx
+++ src/components/AvatarComponent.jsx
@@ -55,25 +55,34 @@ const pickerConfig = {
 
 class AvatarComponent extends Component<Props> {
   // @ autobind
-  handleImageSelected() {
+  handleImageSelected(onChange) {
     ImagePicker.openPicker(pickerConfig).then(
       image => {
         // TODO: create type folder with type 
         // described here https://github.com/ivpusic/react-native-image-crop-picker#response-object
         // TODO: move service call to redux middleware
-        BlobService.getFilePath(image.path).then(this.props.onChange)
+
+        // BlobService.getFilePath(image.path).then(onChange) 
+        // due to it is already local files we don't need to process it via blob storage
+        onChange(image.path);
       }
     );
   }
 
+  wrapperForImageSelection = onChange => () => this.handleImageSelected(onChange);
+
   render() {
+    const { input } = this.props;
+    
+    const value = input.value ? {uri: input.value} : undefined;
+    
     return (
       <TouchableOpacity
-        onPress={this.handleImageSelected}
+        onPress={this.wrapperForImageSelection(input.onChange)}
         style={styles.imageLibraryButton}
       >
         <View style={styles.row}>
-          <Image source={this.props.value} style={styles.photoIcon} />
+          <Image source={value} style={styles.photoIcon} />
           <Text style={styles.imageLibraryText}>Upload photo</Text>
         </View>
       </TouchableOpacity>
